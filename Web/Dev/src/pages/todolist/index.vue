965b097e73efe7bb565432cb51c39651c0c2297a
kings <530675800@qq.com>
2017-10-23 17:24:22 +0800

    添加事件列表

--- Web/Dev/src/pages/todolist/index.vue
+++ Web/Dev/src/pages/todolist/index.vue
@@ -37,7 +37,99 @@
 		</aside>
 
 		<main class="todolist-events-mod">
-			
+			<header class="todoEvents-day-header">
+				<div class="todoEvt-headerDay-inner">
+					<div class="title">{{ evt_HeaderTime.date }}</div>
+					<div class="inner">
+						<p>{{ evt_HeaderTime.dayFormat }}</p>
+						<p>{{ evt_HeaderTime.year }} 年 {{ evt_HeaderTime.month }} 月</p>
+					</div>
+				</div>
+				<div class="hd-btns-box">
+					<button class="tbtn add-todo-event" @click="addOneEvent"  :disabled="createNewEvent">
+						<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12">
+							<path d="M12 9H7v5H5V9H0V7h5V2h2v5h5z"></path>
+						</svg>
+					</button>
+				</div>
+			</header>
+
+			<section class="todo-list-box">
+				<div class="no-work-plane" v-show="eventsHelpBox.show">
+					<h3>没有工作安排!</h3>
+					<p class="remid-msg">{{ eventsHelpBox.mes }}</p>
+				</div>
+
+				<ul>
+					<li v-for="(event, index) in events"
+						class="event-rows"
+						:data-id="event.id" 
+						:data-index="index"
+						data-parent="${data.parent}" 
+						data-time="${data.startTime}"
+						@mouseenter="mouseOver"
+						@contextmenu.prevent="quickSetEvent"
+					>
+					<div class="header">
+						<label>
+							<input class="is-done" type="checkbox" :checked="event.complete" @click="updateCheck(index, $event)">
+						</label>
+						<div class="title-box">
+							<input class="title" 
+								placeholder=" 输入你的事件吧 ;)" 
+								:value="event.title" 
+								@keyup="insertData('title', index, $event)"
+								@blur="blurTitle"
+								v-focus="event.title"
+							/>
+						</div>
+						<span class="li-btns-box">
+							<button 
+								@click="showEventsInfo" 
+								class="tbtn arrow-ico down-arrow"
+								:data-parentIndex="index"
+								:data-parentId="event.id"
+							></button>
+						</span>
+					</div>
+					<ul class="inner">
+						<dl class="event-make-col">
+							<dt>开始:</dt>
+							<dd>
+								<p class="date-select-ui" @click="changeEventDate('stime', index, $event)">
+									{{event.stimeF}}
+								</p>
+							</dd>
+						</dl>
+						
+						<dl v-show="event.etimeF" class="event-make-col">
+							<dt>结束:</dt>
+							<dd>
+								<p 
+									class="date-select-ui"
+									@click="changeEventDate('etime', index, $event)"
+								>
+									{{event.etimeF}}
+								</p>
+							</dd>
+						</dl>
+						
+						<dl>
+							<dt>备注:</dt>
+							<dd>
+								<textarea 
+									@keyup="insertData('inner', index, $event)" 
+									class="inner-box"
+									@focus="removeTextAreaAni"
+									@blur="resetTextAreaAni"
+								>{{event.inner}}</textarea>
+							</dd>
+						</dl>
+					</ul>
+					</li>
+				</ul>
+			</section>
+
 		</main>
 		
 	</section>
--- Web/Dev/src/pages/todolist/main.js
+++ Web/Dev/src/pages/todolist/main.js
@@ -21,8 +21,32 @@ export default {
 
 			/* 日历相关 */
 			calendar_pickTime: {},
-			calendar_events: []
+			calendar_events: [],
+
+			/* 事件功能 */
+			evt_HeaderTime: {},
+			events: [],
+			// 当前事件
+			currentEventIndex: -1,
+			// 编辑文件索引 用于确认编辑是那条事件
+			editionEvtIndex: -1,
+
+			thisSetTimeoutFun: '',
+
+			createNewEvent: false,
+			// 事件帮助显示层
+			eventsHelpBox: {
+				show: false
+			}
 			
+		}
+	},
+	directives: {
+		focus: {
+			inserted: function (el, binding) {
+				if (!binding.value)
+					el.focus()
+			}
 		}
 	},
 	beforeCreate: function() {
@@ -78,7 +102,7 @@ export default {
 		/* 日历事件变化监听功能 */
 		calendar_pickTime (val, old) {
 			// 更新 事件列表的日期
-			// todoEventsListApp.headerTime = val;
+			this.evt_HeaderTime = val;
 
 			if (val.from !== 'auto') getEvents(this);
 
@@ -247,6 +271,7 @@ export default {
 				
 			}, err => {
 				console.error(err)
+
 			})
 		},
 
@@ -254,18 +279,18 @@ export default {
 		updateCalendarEvent (addArr, delArr) {
 
 			let add = day => {
-				if (!this.events[day]) {
-					this.$set(this.events, day, 0)
+				if (!this.calendar_events[day]) {
+					this.$set(this.calendar_events, day, 0)
 				}
 
-				let num = this.events[day] + 1;
-				this.$set(this.events, day, num)
+				let num = this.calendar_events[day] + 1;
+				this.$set(this.calendar_events, day, num)
 			}
 
 			let del = day => {
-				if (!this.events[day]) return;
+				if (!this.calendar_events[day]) return;
 				
-				this.events[day] -= 1
+				this.calendar_events[day] -= 1
 			}
 
 			addArr.forEach(val => {
@@ -276,6 +301,331 @@ export default {
 				del(val)
 			})
 		},
+
+
+		/* 事件功能区 */
+
+		showEventsInfo: function(evt) {
+			
+			let _ = evt.target;
+			let index = _.dataset.parentindex;
+			let parentEl = document.querySelector(`[data-id="${_.dataset.parentid}"]`);
+			let innerEl  = parentEl.querySelector('.inner')
+			let textareaEl = innerEl.querySelector('.inner-box')
+			let textareaH = textareaEl.scrollHeight;
+
+			if (this.events[index].showInfo) {
+				_.classList.remove('show');
+				innerEl.style.height = 0;
+				this.events[index].showInfo = false;
+			}
+			else {
+				_.classList.add('show');
+
+				textareaEl.style.height = textareaH + 'px';
+
+				let innerH = innerEl.scrollHeight;
+
+				innerEl.style.height = innerH+'px';
+
+				this.events[index].showInfo = true
+			}
+
+		},
+
+		// 添加事件
+		addOneEvent: function() {
+
+			let _pickTime = this.calendar_pickTime
+
+			// 如果没有在创建就创建
+			if (this.createNewEvent) {
+				return;
+			};
+
+			let nowTime = new Date()
+			let saveDefTime = new Date(
+				_pickTime.year,
+				_pickTime.month -1,
+				_pickTime.date,
+				nowTime.getHours(),
+				nowTime.getMinutes(),
+				nowTime.getSeconds(),
+				nowTime.getMilliseconds()
+			);
+			let insertData = formatEventData([{
+				insert: true,
+				id: nowTime.getTime(),
+				title: '',
+				complete: false,
+				eventTypeID: this.typeList[ this.holdTypeIndex ].id,
+				inner: '',
+				ctime: saveDefTime,
+				stime: saveDefTime,
+				etime: saveDefTime
+			}])[0];
+
+			
+			// 绑定当前事件索引
+			this.currentEventIndex = this.events.length
+
+			this.createNewEvent = true;
+
+			this.events.push( insertData )
+
+		},
+
+
+		// 时时跟踪当前事件索引
+		mouseOver: function(evt) {
+			this.currentEventIndex = evt.target.dataset.index;
+		},
+
+		// 更新完成情况
+		updateCheck: function(index, evt) {
+
+			this.editionEvtIndex = index;
+			this.events[index].complete = evt.target.checked;
+
+			this.saveInsertData()
+		},
+
+		// 如果新建的标题没有内容,我们就撤消
+		blurTitle: function(evt) {
+
+			if (!evt.target.value && this.createNewEvent) {
+			console.log(evt.target.value);
+				// 撤消新数据
+				this.events.pop();
+				// 恢复可新加
+				this.createNewEvent = false;
+			}
+		},
+
+		// 移除输入框高度动画
+		removeTextAreaAni: function(evt) {
+			evt.target.parentElement.parentElement.parentElement.style.transitionDuration = '0s'
+		},
+
+		// 恢复输入框高度动画
+		resetTextAreaAni: function(evt) {
+			evt.target.parentElement.parentElement.parentElement.style.transitionDuration = '0.3s'
+		},
+
+		// 添加插入数据更新
+		/*
+			@type [string] 说明保存的内容
+			@index [number] 当前数据索引
+			@evt 事件
+		*/
+		insertData: function(type, index, evt) {
+			let _ = evt.target;
+			let _innerBox = _.parentElement.parentElement.parentElement;
+			let val = evt.target.value;
+
+			if (type === 'inner') {
+				_innerBox.style.height = 0;
+				_.style.height = 0;
+
+				_.style.height = _.scrollHeight + 'px';
+				_innerBox.style.height = _innerBox.scrollHeight + 'px'
+			}
+
+			this.events[index][type] = val;
+
+			this.editionEvtIndex = index;
+
+			clearTimeout(this.thisSetTimeoutFun);
+
+			this.thisSetTimeoutFun = setTimeout(this.saveInsertData, 1000)
+		},
+
+		// 保存新加数据
+		saveInsertData: function(callback) {
+
+			let eventData = this.events[this.editionEvtIndex];
+
+			let setQueryData = (arr) => {
+
+				let queryData = [];
+				for (let key in eventData) {
+					if (eventData.hasOwnProperty(key)) {
+						// 过滤 insert
+						if ( arr.includes(key) ) {
+
+							let _inner = '';
+
+							switch (key) {
+								case 'complete':
+									_inner = eventData[key];
+									break;
+
+								case 'inner':
+									_inner = eventData[key].replace(/\n|\r/ig,'\\n');
+									_inner = `"${_inner}"`
+
+									break;
+
+								default:
+									_inner = `"${eventData[key]}"`;
+							}
+							queryData.push( `${key}:${_inner}` )
+
+						}
+					}
+				}
+
+				return queryData.join(',');
+			}
+
+			if (!eventData.title.trim()) return;
+
+			let updateQ = setQueryData(['eventTypeID', 'title', 'complete', 'inner', 'stime', 'etime'])
+
+			let query = `mutation { 
+				saveTodoListEvent(
+					id: "${eventData.id}", 
+					account: "MY_ACCOUNT", 
+					data: { ${updateQ} }
+				) { id, addTime { time {time,day} }, delTime { time{time,day} }, save }
+			}`
+
+			APIFetch({ query }).then( data => {
+				
+				data = data.saveTodoListEvent;
+				let saveInfo = JSON.parse(data.save)
+
+				if (saveInfo.ok && saveInfo.n) {
+
+					// 新建情况
+					if (saveInfo.upserted) {
+						// 删除新建标识
+						delete eventData.insert;
+						// 更新 id
+						eventData.id = data.id
+					}
+
+					// 恢复可以新加功能
+					this.createNewEvent = false;
+
+					let addTimeArr = filterTimeArr(data.addTime.time, this.calendar_pickTime)
+					let delTimeArr = filterTimeArr(data.delTime.time, this.calendar_pickTime)
+
+					// 添加日历
+					this.updateCalendarEvent(addTimeArr, delTimeArr)
+
+				} else {
+					console.error( data )
+				}
+
+				if (callback) callback(data)
+
+			}, err => {
+				console.error(err)
+			})
+		},
+
+		// 右键功能
+		quickSetEvent: function() {
+
+			let index = this.currentEventIndex;
+			let thisID = this.events[this.currentEventIndex].id;
+
+			let moveSubMenu = todolistType.typeList.map((val, i) => {
+				let isSelf = i === todolistType.holdTypeIndex ? true : false;
+				return {
+					label: val.name,
+					type: 'checkbox',
+					checked: isSelf,
+					enabled: !isSelf,
+					click() {
+
+						let _index = todoEventsListApp.currentEventIndex;
+						let _event = todoEventsListApp.events[_index];
+
+						_event.eventTypeID = val.id; 
+
+						todoEventsListApp.saveInsertData(function(data) {
+							// 如果移动成功,更新
+							if (data.ok && data.n) {
+								todoEventsListApp.events.splice(_index, 1)
+							}
+						})
+						
+					}
+				}
+			})
+
+			let menuArr = [
+				{
+					label: '设置',
+					submenu: moveSubMenu
+				},
+				{
+					type: 'separator'
+				},
+				{
+					// 删除类型
+					label: '删除',
+					click() {
+						
+						APIFetch({
+							query: `mutation {
+								removeTodoListEvent(id: "${thisID}", account: "MY_ACCOUNT") {save, time {day, time}}
+							}`
+						}).then(data => {
+							data = data.removeTodoListEvent;
+							let saveInfo = JSON.parse(data.save)[0]
+
+							// 如果有删除数据
+							if (saveInfo.n && saveInfo.ok) {
+								// 删除数据
+								todoEventsListApp.events.splice(index, 1)
+
+								// 更新日历
+								eventsCalendarMod.updateCalendarEvent([],data.time[0].day.split(','))
+							} 
+						}, err => {
+							console.error(err)
+						})
+					}
+				}
+			];
+
+			// 生成菜单
+			createMouseRightClickMenu( menuArr );
+		},
+
+
+		/*
+			时间选择
+			@type [start|end] 开始或结束时间
+			@index [number] 当前数据索引
+			@evt 事件
+		*/
+		changeEventDate: function(type, index, evt) {
+			// 设置插件默认时间
+			eventChangeDateMod.defVal = this.events[index][type];
+
+			this.editionEvtIndex = index;
+			
+			// 获取点击的位置信息
+			let evtRect = evt.target.getBoundingClientRect();
+			let top = evtRect.top;
+
+			top = top + 250 > window.innerHeight ?
+				  window.innerHeight - 250 :
+				  top;
+
+			eventChangeDateMod.rectInfo.top = top
+
+			// 输出点击时间的类型
+			eventChangeDateMod.saveTimeType = type;
+			// 显示时间插件
+			eventChangeDateMod.show = true;
+
+		}
+	
 	}
 }
 
@@ -309,6 +659,10 @@ async function getEvents (that) {
 	if (!result.todolistEvetns.length) {
 		console.error('Error! 没有发现数据!!');
 		that.events = []
+		that.eventsHelpBox = {
+			show: true,
+			mes: '小技巧: 你可以点击右上角的加号,自行添加哟!'
+		}
 		return;
 	}
 
@@ -349,3 +703,31 @@ function formatEventData(obj) {
 	})
 }
 
+/*
+	过滤日历,只找到与日历相同的
+	-----------------------------------
+	eg:
+	[
+		{
+			"time": "2017-9",
+			"day": "30"
+		}
+	]
+	=> ['30']
+*/
+function filterTimeArr (arr, pickTime) {
+	let result = [];
+	let calendarNowTime = `${pickTime.year}-${pickTime.month}`;
+
+	if (arr) {
+		result = arr.filter(val => {
+			// 如果是和日历同月的就显示事件
+			if (val.time === calendarNowTime) 
+			return val
+		})
+
+		result = result[0].day.split(',');
+	}
+	
+	return result;
+}
