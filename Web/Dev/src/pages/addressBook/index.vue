eafc932f6d05f924f558fbcd8af49f196d399c31
ektx <530675800@qq.com>
2017-11-28 14:33:20 +0800

    通讯录日常开发

--- Web/Dev/src/pages/addressBook/index.vue
+++ Web/Dev/src/pages/addressBook/index.vue
@@ -3,9 +3,9 @@
 		<aside class="address-user-box">
 
 			<header>
-				<input type="text" placeholder="通讯录" disabled>
+				<input type="text" placeholder="Search...">
 				
-				<button class="tbtn">
+				<button class="tbtn" @click="addAUser">
 					<svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12">
 						<path d="M12 9H7v5H5V9H0V7h5V2h2v5h5z"></path>
 					</svg>
@@ -14,18 +14,17 @@
 			
 			<div class="users-list-mod">
 				<ul class="user-list-box">
-					<li v-for="(usr, key) in users" 
-						:class="[key === holdIndex ? 'current' : '']"
+					<li v-for="(usr, index) in users" 
+						:class="[index === holdIndex ? 'current' : '']"
 						:data-id="usr.id"
-						@click="holdThisUsr(key)"
+						@click="holdThisUsr(index)"
 					>
 						<figure class="user-icon">
-							<img v-if="usr.icon.length" :src="usr.icon" alt="">
+							<img v-if="usr.icon && usr.icon.length" :src="usr.icon" alt="">
 							<div v-else class="no-user-icon">{{ usr.name.slice(0,1) }}</div>
 						</figure>
 						<dd>
 							<dt>{{ usr.name }}</dt>
-							<dl>{{ usr.mood }}</dl>
 						</dd>
 					</li>
 				</ul>
@@ -34,20 +33,43 @@
 		</aside>
 
 		<div class="user-info-mod">
+			<header>
+				<button class="btn" @click="isEdit = !isEdit">{{ isEdit ? '完成':'编辑'}}</button>
+			</header>
 			<figure>
 				<img v-if="user.icon && user.icon.length" :src="user.icon" :alt="[user.name + ' 头相']">
 				<div v-else-if="user.name && user.name.length" class="no-user-icon">{{ user.name.slice(0,1) }}</div>
 
 			</figure>
-			<h3>{{ user.name }}</h3>
-			<h5>{{ user.mood }}</h5>
+			<h3><input type="text" v-model="user.name" :readonly="!isEdit" placeholder="姓名"></h3>
 
-			<div class="user-contact-list" v-for="val in user.contact">
-				<dl>
-					<dt>{{ val.name }}</dt>
-					<dd><input type="text" readonly="readonly" :value="val.data"></dd>
+			<div class="user-contact-list">
+				<dl v-for="(val, index) in user.contact">
+					<dt>
+						<span v-show="!isEdit">{{val.name}}</span>
+						<input v-show="isEdit" type="text" :value="val.name" :readonly="!isEdit"></dt>
+					<dd>
+						<input type="text" :value="val.data" :readonly="!isEdit">
+						<button class="del-data" v-show="isEdit"></button>
+					</dd>
 				</dl>
 			</div>
+
+			<div class="user-contact-list" style="display: none">
+				<dl>
+					<dt>
+						<span>备注</span>
+					</dt>
+					<dd><textarea></textarea></dd>
+				</dl>
+			</div>
+
+			<transition name="slide">
+				<footer v-show="isEdit">
+					<button @click="addOneData">添加一条数据</button>
+					<button class="err-style" @click="delUser(user)">删除</button>
+				</footer>
+			</transition>
 		</div>
 	</section>
 </template>
--- Web/Dev/src/pages/addressBook/main.js
+++ Web/Dev/src/pages/addressBook/main.js
@@ -5,7 +5,8 @@ export default {
 		return {
 			users: [],
 			user: {},
-			holdIndex: -1
+			holdIndex: -1,
+			isEdit: false
 		}
 	},
 	created() {
@@ -13,8 +14,12 @@ export default {
 	},
 	watch: {
 		holdIndex (val, old) {
+console.log(this.user)
 			this.user = this.users[val]
+		},
 
+		user (val, old) {
+			this.users[this.holdIndex] = val
 		}
 	},
 	methods: {
@@ -30,7 +35,45 @@ export default {
 
 		// 选中用户效果
 		holdThisUsr (index) {
+
+			if (this.isEdit) {
+				this.isEdit = false
+			}
+			
 			this.holdIndex = index
+		},
+
+		// 删除用户
+		delUser (user) {
+			console.log(user)
+		},
+
+		addAUser () {
+			console.log('add a user')
+
+			this.isEdit = true;
+
+			let data = {
+				id: 'user' +(+ new Date()),
+				name: '',
+				contact: [{
+					type: 'tel',
+					name: '电话',
+					data: ''
+				}]
+			};
+
+			this.users.unshift(data)
+			this.user = data
+
+			// this.users.pop()
+console.log(this.users)
+			
+			this.holdIndex = 0;
+		},
+
+		addOneData () {
+			console.log('new data!')
 		}
 	}
 }
