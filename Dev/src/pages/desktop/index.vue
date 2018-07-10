abd4cc232452d740b8d41b475f912cdf78f86612
祝文龙 <zhuwenlong@hztianque.com>
2018-07-10 21:58:43 +0800

    对桌面添加链接

--- Dev/src/pages/desktop/index.vue
+++ Dev/src/pages/desktop/index.vue
@@ -1,5 +1,15 @@
 <template>
-    <div class="workman-desktop"></div>
+    <div class="workman-desktop">
+        <header>
+            <h1>WorkMan</h1>
+        </header>
+        <ul>
+            <li v-for="(item, index) in navs" :key="index">
+                <router-link :to="item.to">{{item.title}}</router-link>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script src="./main.js"></script>
+<style lang="less" scoped src="./layout.less"></style>
