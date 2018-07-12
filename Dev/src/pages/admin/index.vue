8bbde4c4662093a3837ba7b48185829dd8a8d8b0
祝文龙 <zhuwenlong@hztianque.com>
2018-07-12 21:58:02 +0800

    add admin page

--- Dev/src/pages/admin/index.vue
+++ Dev/src/pages/admin/index.vue
@@ -1,7 +1,21 @@
 <template>
     <section class="admin-page">
-        <h1>WorkMan Admin</h1>
+        <header>
+            <h1>WorkMan Admin</h1>
+            <p>欢迎使用 WorkMan! 请设置你的系统管理员!</p>
+        </header>
 
+        <Form class="form-mod" ref="form" :model="admin" :rules="rulesVal">
+            <FormItem label="邮箱" prop="email">
+                <Input v-model="admin.email" placeholder="请输入邮箱"/>
+            </FormItem>
+            <FormItem label="密码" prop="pass">
+                <Input v-model="admin.pass" placeholder="请输入密码"/>
+            </FormItem>
+            <FormItem>
+                <Button type="primary" @click="save">保存</Button>
+            </FormItem>
+        </Form>
     </section>
 </template>
 
@@ -10,7 +24,28 @@ export default {
     name: 'admin',
     data () {
         return {
-
+            admin: {
+                email: '',
+                pass: ''
+            },
+            rulesVal: {
+                email: [
+                    { required: true, message: '不能为空', trigger: 'blur' },
+                    { type: 'email', message: '邮箱格式不正确', trigger: 'blur'}
+                ],
+                pass: [
+                    {
+                        required: true,
+                        message: '密码不能为空',
+                        trigger: 'blur'
+                    }
+                ]
+            }
+        }
+    },
+    methods: {
+        save () {
+            console.log(this.admin)
         }
     }
 }
@@ -26,6 +61,11 @@ export default {
     h1 {
         font-weight: 400;
         color: #333;
+    }
+
+    .form-mod {
+        margin: 2em 0;
+        max-width: 400px;
     }
 }
 
