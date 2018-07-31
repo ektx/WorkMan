bf6cdc9d7ecc0fb9243a917bff2fe715d7fddd5f
ektx <530675800@qq.com>
2018-07-31 16:27:46 +0800

    完善系统管理员添加功能

--- Dev/src/pages/admin/index.vue
+++ Dev/src/pages/admin/index.vue
@@ -1,22 +1,37 @@
 <template>
     <section class="admin-page">
-        <header>
-            <h1>WorkMan Admin</h1>
-            <p>欢迎使用 WorkMan! 请设置你的系统管理员!</p>
-        </header>
+        <div v-if="show" class="set-admin-box">
+            <header>
+                <h1>WorkMan Admin</h1>
+                <p>欢迎使用 WorkMan! 请设置你的系统管理员!</p>
+            </header>
 
-        <Form class="form-mod" ref="form" :model="admin" :rules="rulesVal">
-            <FormItem label="邮箱" prop="email">
-                <Input v-model="admin.email" placeholder="请输入邮箱"/>
-            </FormItem>
-            <FormItem label="密码" prop="pass">
-                <Input v-model="admin.pass" placeholder="请输入密码"/>
-            </FormItem>
-            <FormItem>
-                <Button type="primary" @click="save">保存</Button>
-                <Button class="reset" @click="reset">重写</Button>
-            </FormItem>
-        </Form>
+            <Form class="form-mod" ref="form" :model="admin" :rules="rulesVal">
+                <FormItem label="邮箱" prop="email">
+                    <Input v-model="admin.email" placeholder="请输入邮箱"/>
+                </FormItem>
+                <FormItem label="密码" prop="pass">
+                    <Input type="password" v-model="admin.pass" placeholder="请输入密码"/>
+                </FormItem>
+                <FormItem>
+                    <Button type="primary" @click="save">保存</Button>
+                    <Button class="reset" @click="reset">重写</Button>
+                </FormItem>
+            </Form>
+        </div>
+
+        <div v-if="!show" class="display-admin-box">
+            <header>
+                <h1>WorkMan Admin</h1>
+                <p>欢迎使用 WorkMan! 以下为系统管理员!</p>
+            </header>
+            <ul class="display-panel">
+                <li v-for="item in list" :key="item.account">
+                    <h3>{{item.account}} <span v-if="item.name">({{item.name}})</span></h3>
+                    <p>{{item.email}}</p>
+                </li>
+            </ul>
+        </div>
     </section>
 </template>
 
@@ -41,14 +56,42 @@ export default {
                         trigger: 'blur'
                     }
                 ]
-            }
+            },
+            list: [],
+            show: false
         }
     },
+    mounted: function () {
+        this.findAdminList()
+    },
     methods: {
+        findAdminList () {
+            this.$axios({
+                url: '/api/v1/findAdminList',
+                method: 'get'
+            }).then(res => {
+                if (res.error || !res.list.length) {
+                    this.show = true
+                } else {
+                    this.list = res.list
+                }
+            })
+        },
+
         save () {
             this.$refs.form.validate(valid => {
                 if (valid) {
-
+                    this.$axios({
+                        url: '/api/v1/saveAdmin',
+                        method: 'post',
+                        data: this.admin
+                    }).then(res => {
+                        if (res.success) {
+                            this.$Message.success(res.data)
+                        } else {
+                            this.$Message.error(res.data)
+                        }
+                    })
                 } else {
                     this.$Message.error('请确认表单内容')
                 }
@@ -88,6 +131,19 @@ export default {
 
     .reset {
         margin-left: 10px;
+    }
+
+    .display-panel {
+        width: 300px;
+        margin: 60px 0 100px;
+        background: #fff;
+        border: 1px solid #ddd;
+        border-radius: 3px;
+        overflow: hidden;
+
+        li {
+            padding: 5px 10px;
+        }
     }
 }
 
